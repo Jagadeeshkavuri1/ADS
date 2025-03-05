662d7012ca2d36b90de8dec4812d5636a57a2e85
KAVURI VEERA VENKATA JAGADEESH <145770710+Jagadeeshkavuri1@users.noreply.github.com>
2025-03-06 00:11:23 +0530

    Update knapsack.java

--- knapsack.java
+++ knapsack.java
@@ -1,8 +1,5 @@
-
 import java.util.*;
-
 public class knapsack {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter bag weight");
@@ -11,38 +8,58 @@ public class knapsack {
         int n = sc.nextInt();
         int p[] = new int[n];
         int w[] = new int[n];
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.println("enter" + i + "object weight");
             w[i] = sc.nextInt();
             System.out.println("enter" + i + "object profit");
             p[i] = sc.nextInt();
         }
         double rato[] = new double[n];
-        int fw[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        double fw[] = new double[n];
+        for (int i = 0; i < n; i++) {  
             rato[i] = (double) p[i] / w[i];
         }
         Arrays.fill(fw, 0);
         double temp;
-        int teemp;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n-1; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (rato[i] > rato[j]) {
+                if (rato[i] < rato[j]) {
                     temp = rato[i];
                     rato[i] = rato[j];
                     rato[j] = temp;
-                    teemp = p[i];
+                    temp = p[i];
                     p[i] = p[j];
-                    p[j] = teemp;
-                    teemp = w[i];
+                    p[j] = (int)temp;
+                    temp = w[i];
                     w[i] = w[j];
-                    w[j] = teemp;
+                    w[j] = (int)temp;
 
                 }
             }
-
+        } 
+        double maxpro=0; 
+        for(int i=0;i<n;i++){  
+            if(m==0){ 
+            break;
+            }
+            else if(m>w[i]){ 
+                maxpro +=p[i]; 
+                m -=w[i]; 
+                fw[i]=1;  
+                System.out.println("Item Weight "+w[i]+" with profit "+p[i]+" placed fully in the bag");
+            } 
+            else{  
+                fw[i]=(double)m/w[i];  
+                m=0;
+                maxpro+=p[i]*fw[i];  
+                System.out.println("weighgt of the object is"+w[i]+"profit is"+p[i]+"we placed weight of the object is"+fw[i]);  
+            }
+        } 
+        System.out.println("maximum profit is "+maxpro);  
+        for(int i=0;i<n;i++){ 
+            System.out.println(fw[i]);
         }
-
+        sc.close();
     }
 
 }
